ceedfdea6c059cfaad16086772d670c4838ba37c
worthingtonjg <worthingtonjg@gmail.com>
2022-01-29 10:24:00 -0700

    parts

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumPlayer
+{
+    Unassigned,
+    P1,
+    P2
+}
+
+public enum EnumPartShape
+{
+    Cube,
+    Heart,
+    Pyramid,
+    Torus,
+}
+
+public enum EnumPartColor
+{
+    Blue,
+    Green,
+    Red,
+    Yellow,
+}
